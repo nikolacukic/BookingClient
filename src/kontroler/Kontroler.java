92b0a31faccd04810a7dccd64e8e175f9fb522df
Nikola PC <Nikola PC@DESKTOP-4PAF5EL>
2019-04-23 16:39:41 +0200

    Uradjena metoda za registraciju u klijentskom kontroleru.

--- src/kontroler/Kontroler.java
+++ src/kontroler/Kontroler.java
@@ -62,7 +62,35 @@ public class Kontroler {
         throw ex;
     }
 
-    public Korisnik registracija(HashMap<String, String> recnik) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Korisnik registracija(HashMap<String, String> recnik) throws Exception {
+        String ime = recnik.get("ime");
+        String prezime = recnik.get("prezime");
+        String JMBG = recnik.get("JMBG");
+        String username = recnik.get("username");
+        String password = recnik.get("password");
+        String email = recnik.get("email");
+        String licnaKarta = recnik.get("licnaKarta");
+        String telefon = recnik.get("telefon");
+        if (!licnaKarta.isEmpty()) {
+            VlasnikSmestaja vlasnik = new VlasnikSmestaja(licnaKarta, telefon, 0, username, password, ime + " " + prezime, JMBG, email);
+            Zahtev zahtev = new Zahtev(Operacije.LOGIN, vlasnik);
+            Komunikacija.getInstance().saljiZahtev(zahtev);
+            Odgovor odgovor = Komunikacija.getInstance().citajOdgovor();
+            if (odgovor.getStatus() == StatusOdgovora.OK) {
+                return (Korisnik) odgovor.getPodaci();
+            }
+            Exception ex = (Exception) odgovor.getError();
+            throw ex;
+        } else {
+            Klijent k = new Klijent(0, 0, username, password, ime + " " + prezime, JMBG, email);
+            Zahtev zahtev = new Zahtev(Operacije.REGISTRACIJA, k);
+            Komunikacija.getInstance().saljiZahtev(zahtev);
+            Odgovor odgovor = Komunikacija.getInstance().citajOdgovor();
+            if (odgovor.getStatus() == StatusOdgovora.OK) {
+                return (Korisnik) odgovor.getPodaci();
+            }
+            Exception ex = (Exception) odgovor.getError();
+            throw ex;
+        }
     }
 }
